40a3ae58b8aec62acbea7798c1846d96d6f34b21
PitToYondeKudasai <buttaronipeter@gmail.com>
2020-05-03 11:20:30 +0200

    Create allmodelsinaplot.m

---
+++ Assignment 5 Codici/allmodelsinaplot.m
@@ -0,0 +1,7 @@
+hr = figure;
+plot(data(:,1), data(:,2), '-bo','MarkerEdgeColor','r','LineWidth', 2, 'DisplayName', 'Original points')
+title('World Crude Oil Production by Year');
+xlabel('Year');
+ylabel('Thousand Barrels per Day');
+
+figure;
